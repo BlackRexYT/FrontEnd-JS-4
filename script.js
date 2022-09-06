9a33385dc3366dafbda3c4d4ecf69f760398f7b3
Reks <fahadadsani@gmail.com>
2022-09-06 11:43:55 +0300

    commit 3

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 /* 1 */
 
 
-function showRating(num){
-    let ratings = ''
-    for (i=0;i<Math.floor(num);i++){
-        ratings += '*'
-        if (i !== Math.floor(num) - 1){
-            ratings += ' '
-        }
-    }
-    if (!Number.isInteger(num)){
-        ratings += ' .'
-    }
-    return ratings
+// function showRating(num){
+//     let ratings = ''
+//     for (i=0;i<Math.floor(num);i++){
+//         ratings += '*'
+//         if (i !== Math.floor(num) - 1){
+//             ratings += ' '
+//         }
+//     }
+//     if (!Number.isInteger(num)){
+//         ratings += ' .'
+//     }
+//     return ratings
 
 
 
-    // for (i=0;i<4.5;i++){
-    //     console.log(i)
-    // }
+//     // for (i=0;i<4.5;i++){
+//     //     console.log(i)
+//     // }
 
 
 
-}
+// }
 
-console.log(showRating(4))
+// console.log(showRating(4))
 
 
 /* 2 */
