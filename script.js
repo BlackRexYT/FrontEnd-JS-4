8af0fa3b4ed8e254283ceb700890b673a0640865
Reks <fahadadsani@gmail.com>
2022-09-06 11:52:15 +0300

    commit 5

--- script.js
+++ script.js
@@ -32,8 +32,13 @@
 
 
 
-function sortHighToLow(arr){
-    return arr.sort((a,b) => b.price - a.price)
-}
+// function sortHighToLow(arr){
+//     return arr.sort((a,b) => b.price - a.price)
+// }
 
-console.log(sortHighToLow([{id:1, price:50},{id:2, price:0},{id:3, price:500}]))
+// console.log(sortHighToLow([
+//     {id:1, price:50},
+//     {id:2, price:0},
+//     {id:3, price:500}
+// ])
+// )
